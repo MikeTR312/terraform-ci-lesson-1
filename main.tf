279ffff321de3615d40d673679ca57cb0a00d29d
User <user@MacBookPro.lan>
2025-05-12 13:30:10 -0500

    add S3 bucket for Terraform backend

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "mike-tfstate-bucket-2025"  # Имя должно быть уникальным по AWS
+  force_destroy = true
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
