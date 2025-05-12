508157d022a0494b2176b8475264aa23784dae9c
User <user@MacBookPro.lan>
2025-05-12 13:17:54 -0500

    add second EC2 instance

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ resource "aws_instance" "my_ec2" {
     Name = "MyDevInstance"
   }
 }
+
+resource "aws_instance" "my_ec2_b" {
+  ami           = "ami-0f88e80871fd81e91"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-03a9fea22426a5900"
+
+  tags = {
+    Name = "MyDevInstance-B"
+  }
+}
