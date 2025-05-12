6010cfda7a1776ee1f4d589b222c8ca1df6de462
User <user@Users-MacBook-Pro.local>
2025-05-12 12:10:08 -0500

    clean AWS-only main.tf for CI

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0f88e80871fd81e91"
   instance_type = "t2.micro"
-  subnet_id = "subnet-03a9fea22426a5900"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-  profile = "mini-project-profile"
+  subnet_id     = "subnet-03a9fea22426a5900"
 }
