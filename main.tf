0a4b5da84caf80670e9e6d04bd1d461310c82db5
User <user@MacBookPro.lan>
2025-05-12 13:01:19 -0500

    add Name tag to EC2

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-0f88e80871fd81e91"
   instance_type = "t2.micro"
   subnet_id     = "subnet-03a9fea22426a5900"
+
+  tags = {
+    Name = "MyDevInstance"
+  }
 }
